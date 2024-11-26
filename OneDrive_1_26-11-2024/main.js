240bffdb485c2185a0ad21b545036e1607753b56
turko06 <146332455+turko06@users.noreply.github.com>
2024-11-26 12:39:00 +0000

    Initial commit

---
+++ OneDrive_1_26-11-2024/main.js
@@ -0,0 +1,33 @@
+"use strict"; // Enables strict mode, which catches common coding errors and improves performance
+
+/*
+    	Name: 
+	Banner ID:
+	Course:  ELEE1159
+*/
+
+// Utility function to get a DOM element by selector
+const getElement = selector => document.querySelector(selector);
+
+// image constants 
+const backImgSrc = "images/back.png";
+const blankImgSrc = "images/blank.png";
+const cardImgSrcStart = "images/card_";
+
+document.addEventListener("DOMContentLoaded", () => {
+    // display cards and player info
+    
+
+    // load settings data
+    
+ 
+	// add click event handler for each card link
+
+
+    // add click event handler for each tab link button
+    
+
+    // add click event handler for Save Settings button
+    
+    
+}); 
